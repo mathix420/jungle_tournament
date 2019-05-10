651c14ba2874a94997e222371866bc92972a7737
Arnaud GISSINGER <agissing@e2r9p12.42.fr>
2019-05-10 18:46:29 +0200

    good stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'dart:io';
-
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
+
+List<JungleChoice> cards = [null, null];
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,39 +29,111 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class JungleChoice extends Container {
-  JungleChoice({Key key, this.pos, this.color, this.url, this.onClick})
-      : super(key: key);
+class JungleChoice extends StatefulWidget {
+  JungleChoice(
+      {Key key, this.pos, this.color, this.url, this.votes, this.onTap});
 
   final String pos;
   final String url;
   final Color color;
-  final Function onClick;
+  final String votes;
+  final Function onTap;
+
+  @override
+  _JungleChoiceState createState() => _JungleChoiceState();
+}
+
+class IsBlurred extends InheritedWidget {
+  static of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(IsBlurred);
+
+  final bool isBlurred;
+
+  IsBlurred({Key key, @required Widget child, @required this.isBlurred})
+      : assert(isBlurred != null),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(IsBlurred oldWidget) {
+    return this.isBlurred != oldWidget.isBlurred;
+  }
+}
+
+class _JungleChoiceState extends State<JungleChoice> {
+  NetworkImage fighterImage;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: color,
-        margin: (this.pos == "left")
-            ? new EdgeInsets.fromLTRB(4, 8, 2, 4)
-            : new EdgeInsets.fromLTRB(2, 8, 4, 4),
-        height: 400,
-        child: ClipRRect(
-          borderRadius: new BorderRadius.all(new Radius.circular(8.0)),
-          child: new GestureDetector(
-            onTap: () => this.onClick(),
-            child: new FadeInImage(
-              image: (url != null && url.length > 0)
-                  ? new NetworkImage(url)
-                  : new AssetImage("assets/none.png"),
-              fit: BoxFit.cover,
-              placeholder: new AssetImage("assets/none.png"),
+    if (widget.url != null && widget.url.length > 0) {
+      fighterImage = new NetworkImage(widget.url);
+    }
+    if (!IsBlurred.of(context).isBlurred) {
+      return Expanded(
+        child: Container(
+          color: widget.color,
+          margin: (widget.pos == "left")
+              ? new EdgeInsets.fromLTRB(4, 8, 2, 4)
+              : new EdgeInsets.fromLTRB(2, 8, 4, 4),
+          height: 400,
+          child: ClipRRect(
+            borderRadius: new BorderRadius.all(new Radius.circular(8.0)),
+            child: new GestureDetector(
+              onTap: () => widget.onTap(),
+              child: new FadeInImage(
+                image: (widget.url != null && widget.url.length > 0)
+                    ? fighterImage
+                    : new AssetImage("assets/none.png"),
+                fit: BoxFit.cover,
+                placeholder: new AssetImage("assets/none.png"),
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Expanded(
+        child: Container(
+          color: widget.color,
+          margin: (widget.pos == "left")
+              ? new EdgeInsets.fromLTRB(4, 8, 2, 4)
+              : new EdgeInsets.fromLTRB(2, 8, 4, 4),
+          height: 400,
+          decoration: new ShapeDecoration(
+            shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.all(new Radius.circular(8.0)),
+            ),
+            image: new DecorationImage(
+              image: (widget.url != null && widget.url.length > 0)
+                  ? fighterImage
+                  : new AssetImage("assets/none.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ClipRRect(
+            borderRadius: new BorderRadius.all(new Radius.circular(8.0)),
+            child: new BackdropFilter(
+              filter: new ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+              child: new GestureDetector(
+                onTap: () {},
+                child: new Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.white.withOpacity(0.0),
+                    borderRadius:
+                        new BorderRadius.all(new Radius.circular(8.0)),
+                  ),
+                  child: Center(
+                    child: Text(
+                      widget.votes.toString() + " votes",
+                      style: TextStyle(fontSize: 30, fontFamily: 'Komikaze'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
   }
 }
 
@@ -88,23 +162,69 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: StreamBuilder(
-        stream: Firestore.instance.collection('matchs').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) return CircularProgressIndicator();
-          DocumentSnapshot doc;
-          snapshot.data.documents.forEach((document) {
-            if (document.data['started'] && !document.data['finished']) {
-              doc = document;
-            }
-          });
-          if (doc == null || doc.data['opponents'] == null) {
-            return CircularProgressIndicator();
-          }
-          return JungleHomeStateful(
-              fightersList: doc.data['opponents'],
-              title: doc.data['title'].toString());
-        },
-      ),
+          stream: FirebaseAuth.instance.signInAnonymously().asStream(),
+          builder: (
+            BuildContext context1,
+            AsyncSnapshot<FirebaseUser> snapshot1,
+          ) {
+            if (!snapshot1.hasData)
+              return CircularProgressIndicator(); // need to put an error
+
+            return StreamBuilder(
+                stream: Firestore.instance
+                    .collection('votes')
+                    .document('map')
+                    .snapshots(),
+                builder: (
+                  BuildContext context2,
+                  AsyncSnapshot<DocumentSnapshot> snapshot2,
+                ) {
+                  if (!snapshot2.hasData) return CircularProgressIndicator();
+
+                  if (snapshot2.data.data['users'].keys != null &&
+                      !snapshot2.data.data['users'].keys
+                          .contains(snapshot1.data.uid.toString())) {
+                    Firestore.instance.runTransaction((transaction) async {
+                      Map newData = snapshot2.data['users'];
+                      newData[snapshot1.data.uid.toString()] = [];
+                      await transaction
+                          .update(snapshot2.data.reference, {'users': newData});
+                    });
+                  }
+                  List<dynamic> listOfVotes = snapshot2.data.data['users']
+                      [snapshot1.data.uid.toString()];
+
+                  return StreamBuilder(
+                      stream:
+                          Firestore.instance.collection('matchs').snapshots(),
+                      builder: (
+                        BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot,
+                      ) {
+                        if (!snapshot.hasData)
+                          return CircularProgressIndicator();
+                        DocumentSnapshot doc;
+                        snapshot.data.documents.forEach((document) {
+                          if (document.data['started'] &&
+                              !document.data['finished']) {
+                            doc = document;
+                          }
+                        });
+                        if (doc == null || doc.data['opponents'] == null) {
+                          return CircularProgressIndicator();
+                        }
+                        return JungleHomeStateful(
+                          fightersList: doc.data['opponents'],
+                          title: doc.data['title'].toString(),
+                          loggedInUser: snapshot1.data,
+                          isBlurred: listOfVotes != null &&
+                              listOfVotes.contains(doc.documentID.toString()),
+                          votesDatabase: snapshot2.data,
+                          fightId: doc.documentID.toString(),
+                        );
+                      });
+                });
+          }),
     );
   }
 }
@@ -126,22 +246,48 @@ class ScoreView extends StatelessWidget {
 class JungleHomeStateful extends StatefulWidget {
   final List<dynamic> fightersList;
   final String title;
+  final FirebaseUser loggedInUser;
+  final bool isBlurred;
+  final String fightId;
+  final DocumentSnapshot votesDatabase;
 
-  JungleHomeStateful({this.fightersList, this.title});
+  JungleHomeStateful(
+      {this.fightersList,
+      this.title,
+      this.loggedInUser,
+      this.isBlurred,
+      this.votesDatabase,
+      this.fightId});
 
   @override
   _JungleHomeStatefulState createState() {
     return new _JungleHomeStatefulState(
-        futureFightersList: fightersList, title: title);
+        futureFightersList: fightersList,
+        title: title,
+        isBlurred: isBlurred,
+        votesDatabase: votesDatabase,
+        loggedInUser: loggedInUser,
+        fightId: fightId);
   }
 }
 
 class _JungleHomeStatefulState extends State<JungleHomeStateful> {
   final List<dynamic> futureFightersList;
   final String title;
+  final FirebaseUser loggedInUser;
+  final String fightId;
+  final DocumentSnapshot votesDatabase;
+  bool isBlurred = false;
+
   List<DocumentSnapshot> _fightersList = new List<DocumentSnapshot>();
 
-  _JungleHomeStatefulState({this.futureFightersList, this.title}) {
+  _JungleHomeStatefulState(
+      {this.futureFightersList,
+      this.title,
+      this.loggedInUser,
+      this.isBlurred,
+      this.fightId,
+      this.votesDatabase}) {
     getOpponentsList(this.futureFightersList).then((val) => setState(() {
           _fightersList = val;
         }));
@@ -156,19 +302,48 @@ class _JungleHomeStatefulState extends State<JungleHomeStateful> {
   }
 
   void vote(int index) {
-    print("IN");
-    Firestore.instance.runTransaction((transaction) async {
-      DocumentSnapshot freshSnap =
-          await transaction.get(_fightersList[index].reference);
-      await transaction
-          .update(freshSnap.reference, {'votes': freshSnap['votes'] + 1});
-    });
-    Route route = MaterialPageRoute(builder: (context) => ScoreView());
-    Navigator.pushReplacement(context, route);
+    if (!isBlurred) {
+      setState(() {
+        isBlurred = true;
+      });
+      Firestore.instance.runTransaction((transaction) async {
+        DocumentSnapshot freshSnap =
+            await transaction.get(_fightersList[index].reference);
+        await transaction
+            .update(freshSnap.reference, {'votes': freshSnap['votes'] + 1});
+      });
+      Firestore.instance.runTransaction((transaction) async {
+        DocumentSnapshot freshSnap =
+            await transaction.get(votesDatabase.reference);
+        Map newData = freshSnap.data['users'];
+        newData[loggedInUser.uid.toString()] += [fightId];
+        await transaction.update(freshSnap.reference, {'users': newData});
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    cards[0] = new JungleChoice(
+      pos: "left",
+      url: _fightersList.length == 2
+          ? _fightersList[0].data['image'].toString()
+          : null,
+      onTap: () => vote(0),
+      votes: _fightersList.length == 2
+          ? _fightersList[0].data['votes'].toString()
+          : null,
+    );
+    cards[1] = new JungleChoice(
+      pos: "right",
+      url: _fightersList.length == 2
+          ? _fightersList[1].data['image'].toString()
+          : null,
+      onTap: () => vote(1),
+      votes: _fightersList.length == 2
+          ? _fightersList[1].data['votes'].toString()
+          : null,
+    );
     return Column(
       children: <Widget>[
         Stack(
@@ -178,20 +353,8 @@ class _JungleHomeStatefulState extends State<JungleHomeStateful> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                JungleChoice(
-                  pos: "left",
-                  url: _fightersList.length == 2
-                      ? _fightersList[0].data['image'].toString()
-                      : null,
-                  onClick: () => vote(0),
-                ),
-                JungleChoice(
-                  pos: "right",
-                  url: _fightersList.length == 2
-                      ? _fightersList[1].data['image'].toString()
-                      : null,
-                  onClick: () => vote(1),
-                ),
+                IsBlurred(isBlurred: isBlurred, child: cards[0]),
+                IsBlurred(isBlurred: isBlurred, child: cards[1]),
               ],
             ),
             new Text(
@@ -204,10 +367,9 @@ class _JungleHomeStatefulState extends State<JungleHomeStateful> {
                 color: Colors.yellow[700],
                 shadows: <Shadow>[
                   Shadow(
-                    offset: Offset(10.0, 10.0),
-                    blurRadius: 5.0,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                  ),
+                      offset: Offset(10.0, 10.0),
+                      blurRadius: 5.0,
+                      color: Color.fromARGB(255, 0, 0, 0)),
                 ],
               ),
             ),
